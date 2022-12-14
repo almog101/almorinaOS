a0c54341baf0c03d0e6f8c4931f5abfb85d2a0bb
almog101 <almogba8@gmail.com>
2022-12-14 23:30:20 +0200

    added memory map scan

--- src/c-code/kernel.c
+++ src/c-code/kernel.c
@@ -60,7 +60,7 @@ void help()
 	puts("print - takes a format string a variables. ex. print hello%cworld @\n");
 }
 
-void kernel_main() 
+void shell()
 {
 	initialize_idt64();
 
@@ -87,10 +87,45 @@ void kernel_main()
 
 	putc('\n');
 
+}
 
-	printf("Enter expression: ");
-	char exp[200] = "3-1";
-	//fgets(exp, 200-1);
-	printf("%s = %d\n", exp, eval(exp));
+void kernel_main(unsigned long magic, unsigned long addr) 
+{
+	struct multiboot_tag *tag;
+	unsigned size;
 
+	cls();
+
+	/* Make sure the magic number matches for memory mapping*/
+	if(magic != MULTIBOOT2_BOOTLOADER_MAGIC) {
+		puts("invalid magic number!");
+		return;
+	}
+
+	size = *(unsigned *) addr;
+
+	 for (tag = (struct multiboot_tag *) (addr + 8);
+       tag->type != MULTIBOOT_TAG_TYPE_END;
+       tag = (struct multiboot_tag *) ((multiboot_uint8_t *) tag 
+                                       + ((tag->size + 7) & ~7)))
+    {
+		if (tag->type == MULTIBOOT_TAG_TYPE_MMAP)
+		{
+			multiboot_memory_map_t *mmap;
+			puts("mmap\n");
+
+			            for (mmap = ((struct multiboot_tag_mmap *) tag)->entries;
+                 (multiboot_uint8_t *) mmap 
+                   < (multiboot_uint8_t *) tag + tag->size;
+                 mmap = (multiboot_memory_map_t *) 
+                   ((unsigned long) mmap
+                    + ((struct multiboot_tag_mmap *) tag)->entry_size))
+              printf (" base_addr = 0x%x%x, length = 0x%x%x, type = 0x%x\n",
+                      (unsigned) (mmap->addr >> 32),
+                      (unsigned) (mmap->addr & 0xffffffff),
+                      (unsigned) (mmap->len >> 32),
+                      (unsigned) (mmap->len & 0xffffffff),
+                      (unsigned) mmap->type);
+		}
+	}
 }

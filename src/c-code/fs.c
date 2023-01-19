70e329c807272da65ce6ec5a2791fbd4d7015a7c
Karina Bel <experimentsbel@gmail.com>
2023-01-19 16:15:12 +0200

    add block to inode function done [not checked]

--- src/c-code/fs.c
+++ src/c-code/fs.c
@@ -23,8 +23,10 @@ fs_inode_t* fs_create_inode(fs_superblock_t* device, uint8_t type)
 {
 	int i = 0;
 	for (i = 0; i<device->inodes_count; i++)
+	{
 		if (BITSET_READBIT(device->inodes_bitset, i) == 0)
 			break;
+	}
 	
 	if (i == device->inodes_count)
 		return 0;
@@ -33,6 +35,72 @@ fs_inode_t* fs_create_inode(fs_superblock_t* device, uint8_t type)
 	fs_inode_t* inode = &((fs_inode_t*)device->first_inode)[i];
 	*inode = (fs_inode_t){0};
 	inode->mode = type;
+	inode->block[0] = NULL;
 
 	return inode;
+}
+
+bool ckeck_indirect_pointers(int i, uint32_t* pointer_to_blocks, uint32_t block_to_add)
+{
+	if(i == NULL)
+	{
+		pointer_to_blocks[i] = malloc(uint32_t * 15);
+		pointer_to_blocks[i][0] = block_to_add;
+		pointer_to_blocks[i + 1] = NULL;
+		pointer_to_blocks[i][1] = NULL;
+		return 1;
+	}
+	else
+	{
+		for(int j = 0; j < 15; j++)
+		{
+			if(pointer_to_blocks[i][j] == NULL)
+			{
+				pointer_to_blocks[i][j] = block_to_add;
+				if((j + 1) != 15)
+					pointer_to_blocks[i][j + 1] = NULL;
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
+
+void fill_blocks(uint32_t* pointer_to_blocks, uint32_t block_to_add)
+{
+	int i = 0;
+
+	for(i = 0; i < 12; i++)
+	{
+		if(pointer_to_blocks[i] == NULL)
+		{
+			pointer_to_blocks[i] = block_to_add;
+			pointer_to_blocks[i + 1] = NULL;
+			return;
+		}
+	}
+
+	if(ckeck_indirect_pointers(i, pointer_to_blocks, block_to_add))
+		return;
+
+	if(ckeck_indirect_pointers(++i, pointer_to_blocks, block_to_add))
+		return;
+
+	if(++i == NULL)
+	{
+		pointer_to_blocks[i] = malloc(uint32_t * 15);
+		pointer_to_blocks[i][0] = block_to_add;
+		pointer_to_blocks[i + 1] = NULL;
+		pointer_to_blocks[i][1] = NULL;
+		return;
+	}
+	else
+	{
+		fill_blocks(pointer_to_blocks[i], block_to_add);
+	}
+}
+
+void fs_add_block(fs_inode_t* inode, uint32_t block_pointer) /* probably should be uint32_t pointer [not sure]*/
+{
+	fill_blocks(&(inode->block[0]), block_pointer); // should check if works
 }
--- src/include/fs.h
+++ src/include/fs.h
@@ -12,25 +12,41 @@ enum INODE_TYPE {
 
 typedef struct
 {
-	uint32_t	inodes_count;		/* Inodes count */
-	uint32_t	blocks_count;		/* Blocks count */
-	uint32_t	first_inode;		/* First inode */
-	uint32_t	first_data_block;	/* First Data Block */
-	uint32_t	block_size;		/* Block size */
-	bitset_t	inodes_bitset;		/* Inodes Bitset */
-	bitset_t	blocks_bitset;		/* Blocks Bitset */
+	uint32_t	inodes_count;		/* Inodes count 	*/
+	uint32_t	blocks_count;		/* Blocks count 	*/
+	uint32_t	first_inode;		/* First inode 		*/
+	uint32_t	first_data_block;	/* First Data Block	*/
+	uint32_t	block_size;			/* Block size 		*/
+	bitset_t	inodes_bitset;		/* Inodes Bitset 	*/
+	bitset_t	blocks_bitset;		/* Blocks Bitset 	*/
 } fs_superblock_t;
-
 
 typedef struct
 {
-    uint8_t			mode;		/* File mode */
-    uint32_t			size;		/* Size in bytes */
-    uint32_t			atime;		/* Access time */
-    uint32_t			ctime;		/* Creation time */
-    uint32_t 			mtime;		/* Modification time */
-    uint32_t			block[15];	/* Pointers to blocks */
+    uint8_t		mode;		/* File mode 				*/
+    uint32_t	size;		/* Size in bytes 			*/
+    uint32_t	atime;		/* Access time 				*/
+    uint32_t	ctime;		/* Creation time 			*/
+    uint32_t 	mtime;		/* Modification time 		*/
+    uint32_t	block[15];	/* Pointers to blocks 		*/
+							/* 12 direct pointers		*/
+							/* + 3 indirect pointers	*/
 } fs_inode_t;
 
 void* fs_initialize(int inodes_count, int blocks_count);
 fs_inode_t* fs_create_inode(fs_superblock_t* device, uint8_t type);
+
+void fs_add_block(fs_inode_t* inode, uint32_t block_pointer);
+
+/*
+super-block:
+	list of inodes
+	inode:
+		metadata:
+			general information about a file/directory
+		list of blocks [that belong to 1 file/diractory]
+		blocks:
+			parts of a disc [memory]
+		indirect blocks:
+			point to a block of block pointers [??]
+*/

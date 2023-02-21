fd41f33484c0bb6081f11b8c42e271dceae722cb
almog101 <almogba8@gmail.com>
2023-02-21 19:01:38 +0200

    fixed not compileing

--- src/c-code/locks.c
+++ src/c-code/locks.c
@@ -1,15 +1,15 @@
 #include "locks.h"
-#include <stdlib.h>
+#include "heap.h"
 
 semaphore_t* semaphore_create(int max_count) {
     semaphore_t * semaphore;
- 
+
     semaphore = malloc(sizeof(semaphore_t));
-    if(semaphore != NULL) {
+    if(semaphore != 0) {
         semaphore->max_count = max_count;
         semaphore->current_count = 0;
-        semaphore->first_waiting_task = NULL;
-        semaphore->last_waiting_task = NULL;
+        semaphore->first_waiting_task = 0;
+        semaphore->last_waiting_task = 0;
     }
 
 	return semaphore;
